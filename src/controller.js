89282bc861729a9c76f8fa3ba86b6aaff3fdbf86
LezcanoJorge <yochyoch2015@gmail.com>
2023-10-18 22:29:50 -0300

    modificar usuario

--- src/controller.js
+++ src/controller.js
@@ -27,11 +27,11 @@ async getOne(req, res) {
     const [result] = await pool.query(`DELETE FROM Personas WHERE id=(?)`, [persona.id]);
     resjson({"Registros eliminados":result.affectedRows});
   }
-}
-async add(req, res){
-    const libros = req.body;
-    const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, anopublicacion, isbn) VALUES (?,?,?,?,?)`, [libros.nombre, libros.autor, libros.categoria, libros.anopublicacion, libros.isbn]);
-    res.json({"Id insertado": result.insertId});
-}
 
+  async update(req, res){
+    const libros = req.body;
+    const [result] = await pool.query(`UPDATE libros SET nombre=(?) autor=(?) , categoria=(?) , anopublicacion=(?) , isbn=(?)`, [libros.nombre, libros.autor, libros.categoria, libros.anopublicacion, libros.isbn]);
+    res.json({"Registros actualizados": result.changedRows});
+  }
+}
 export const libros = new LibroController();
