28f188b5d473d822c9f486cee9cb683f3bfd1b12
LezcanoJorge <yochyoch2015@gmail.com>
2023-10-18 22:43:33 -0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,7 @@ import { libros } from './controller.js';
 export const router = Router()
 
 router.get('/libros', libros.getAll);
-router.post('/libro', libros.add);
+router.post('/libros', libros.add);
 router.get('/libros/:id' ,libros.getOne);
+router.put('/libros',libros.update);
+router.delete('/libros',libros.delete);
