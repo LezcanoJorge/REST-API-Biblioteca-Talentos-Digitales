fb3a155526fb7e75ea9267d3b67ba2145d38d401
LezcanoJorge <yochyoch2015@gmail.com>
2023-10-21 15:36:12 -0300

    try catch add

--- src/controller.js
+++ src/controller.js
@@ -7,13 +7,19 @@ async getAll(req, res){
     res.json(result)
 
 }
-async add(req, res){
-    const libros = req.body;
-    const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, anopublicacion, isbn) VALUES (?,?,?,?,?)`, [libros.nombre, libros.autor, libros.categoria, libros.anopublicacion, libros.isbn]);
-    res.json({"Id insertado": result.insertId});
+async add(req, res) {
+  try {
+      const libros = req.body;
+      if (!libros.nombre || !libros.autor || !libros.isbn) {throw new Error("Faltan atributos obligatorios en la solicitud");
+      }
+      const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, anopublicacion, isbn) VALUES (?,?,?,?,?)`,[libros.nombre, libros.autor, libros.categoria, libros.anopublicacion, libros.isbn]);
+      res.json({ "Id insertado": result.insertId });
+  } catch (error) {
+      res.status(400).json({ error: error.message });
+  }
 }
 async getOne(req, res) {
-    const id = req.params.id; // Cambiar req.body.id a req.params.id
+    const id = req.params.id;
     try {
       const [result] = await pool.query('SELECT * FROM libros WHERE id = ?', [id]);
       res.json(result.length > 0 ? result[0] : { error: `Libro no encontrado ${id}` });
@@ -22,11 +28,21 @@ async getOne(req, res) {
       res.status(500).json({ error: 'Error al obtener el libro' });
     }
   }
-  async delete(req, res){
+  async delete(req, res) {
     const libros = req.body;
-    const [result] = await pool.query(`DELETE FROM libros WHERE isbn=(?)`, [libros.isbn]);
-    resjson({"Registros eliminados":result.affectedRows});
+    try {
+      const [result] = await pool.query('DELETE FROM libros WHERE isbn = ?', [libros.isbn]);
+      if (result.affectedRows > 0) {
+        res.json({ "Registros eliminados": result.affectedRows });
+      } else {
+        res.json({ "message": "Ningún registro fue eliminado. ISBN no encontrado." });
+      }
+    } catch (error) {
+      console.error('Error al eliminar el libro:', error);
+      res.status(500).json({ error: 'Error al eliminar el libro' });
+    }
   }
+  
 
   async update(req, res){
     const libros = req.body;
